e35fe788adbfbcd38b4606b3008cf88bea377d75
Shawn Evans <shawnevans328@gmail.com>
2026-02-22 16:02:32 -0500

    msg

--- src/data/exams.ts
+++ src/data/exams.ts
@@ -22,8 +22,32 @@ export const exams: Exam[] = [
     year: "2020",
     questions: [
       {
+        id: "question-1a",
+        title: "question 1a",
+        topics: ["Tracing", "Loops"],
+        prompt: `Trace the variables in the following code:
+        
+         \tint x = 5;
+         int y = 10;
+        \tdo{
+ x++;
+ y--;
+ while (y >8){
+ x = x+2;
+ y = y-3;
+ }
+ } while (x < 10);
+`,
+        explanation: `tba`,
+        solution: `--------------------------------------------------
+x =  |  5  |  6  |  8  |  9  |  10  |
+--------------------------------------------------
+y =  |  10 |  9  |  6  |  5  |  4   |
+--------------------------------------------------`,
+      },
+      {
         id: "question-2",
-        title: "Question 2",
+        title: "question 2",
         topics: ["Arithmetic", "Modulo", "Variables"],
         prompt: `Assume the existence of an int variable called inches. Compute and print how many miles, yards, feet and inches that number represents.\n\nThe rules are: 1 foot equals 12 inches, 36 inches equals 1 yard, and 5280 feet equals 1 mile.\n\nFor example, if int inches = 245897, then the output is:\n\n245897 inches equals 3 miles 1550 yards 1 foot and 5 inches`,
         explanation: `To solve this problem, you must use the modulo and division operator.\n\nStart by dividing inches by the total number of inches in a mile (5280 x 12), then 'strip off' those miles using the modulo operator. We strip off the miles so that we are left with how many yards were in the original variable. You repeat the process when you divide the remaining yards by 36, the number of inches in a yard. This process continues and you eventually arrive at the solution.\n\nThey can ask different variants of this problem. Sometimes it is coins, sometimes it is hours. Remember to start large then work your way down, stripping off units as necessary.`,
@@ -63,7 +87,7 @@ export const exams: Exam[] = [
       },
       {
         id: "question-5a",
-        title: "Question 5a",
+        title: "question 5a",
         topics: ["Max Method", "Nested Method Calls"],
         prompt: `Assume the six integers i, j, k, m, n, and l exist and are all initialized.\n\nAssume there exists a method max3(), which accepts 3 integers as input and returns the largest integer.\n\nWrite one Java statement to find the maximum of the six integers using any combination of Math.max() and max3().`,
         explanation: `Java lacks a max method that accepts an arbitrary number of arguments. The standard Math.max() method only accepts two integers. So, we need to 'cheat' in order to find the maximum of six integers.\n\nIf we use max3 twice, we can find the maximum of (i, j, k) and (m, n, l) separately. After doing this, we send the maximum of those two sets to the two argument accepting Math.max() method. The maximum of those two sets are found and then compared. Logically, the result of a Math.max() call on the maximum of (i,j,k) and (m,n,l) should find the absolute highest number.`,
@@ -71,7 +95,7 @@ export const exams: Exam[] = [
       },
       {
         id: "question-5b",
-        title: "Question 5b",
+        title: "question 5b",
         topics: ["Arithmetic", "Math Methods"],
         prompt: `Write a Java expression that would compute the following:`,
         mathHtml: `
@@ -101,7 +125,7 @@ export const exams: Exam[] = [
       },
       {
         id: "question-6",
-        title: "Question 6",
+        title: "question 6",
         topics: ["Strings", "Loops", "substring", "indexOf"],
         prompt: `You are given a string containing a series of 9 digit zip codes with a dash separating the two parts and one space separating each full zip code from the next one. For example,\n\nString str="11230-1234 11011-3489 07621-8845";\n\nWrite Java code to print the first 5 digits of each zip code followed by the last 4 digits of the zip code as separate values.\n\nFor example:\n11230 1234\n11011 3489\n07621 8845\n\nNOTE: The first part of the full zip code is always 5 digits, the second part is always 4 digits and there's always a dash in between the two parts. Keep in mind that your code must handle a String that has any number of such pairs, not just the three in the example above.`,
         explanation: `We solve this problem easily by using the split method. Split the input string based on spaces, creating an array of zip codes. For each zip code in the array, split it based on the dash, then print the first five & last four numbers of that zip code.`,
@@ -280,7 +304,7 @@ public class Fall2020Question7 {
       },
       {
         id: "question-2",
-        title: "Question 2",
+        title: "question 2",
         topics: ["Scanner", "Strings"],
         prompt: `Write Java code to read strings of lower-case letters from the keyboard and count the number of vowels in each word.\n\n(vowels are a, e, i, o and u).\n\nWhen all strings have been read in, print the string that has the largest number of vowels (duplicates included) and how many vowels were in the string. When more than one string has the greatest number of vowels, print the first string found with that number.\n\nFor example,\n\nGiven input: she of groceries yourselves here radio\n\nThe output is: groceries – 4 vowels`,
         explanation: `The solution, for the most part, is a direct regurgitation of the prompt. Start by creating a Scanner. Create an empty string that will represent the string with the most vowels. Create a tracker that remembers how many vowels were in the string with the most vowels.\n\nCreate a while loop bounded by the scanner hasNext() method, a boolean method telling you if there is anything more to read from the input stream. Read in the current string and create a variable representing how many vowels are inside of it. Next, create a for loop to count how many vowels are in that string.\n\nUse an if statement that checks if this string has more vowels than the tracker for the highest vowel count string. If it does, update the tracker.\n\nAt the program's end, print the highest vowel string and how many vowels it had.`,
@@ -330,7 +354,7 @@ public class Spring2021Question2 {
       },
       {
         id: "question-5",
-        title: "Question 5",
+        title: "question 5",
         topics: ["Arrays"],
         prompt: "Question prompt goes here.",
         explanation: "Explanation goes here.",
