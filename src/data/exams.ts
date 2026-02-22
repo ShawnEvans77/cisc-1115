245ccebb204ef4a4ae3826c134ce8e1ca03142a0
Shawn Evans <shawnevans328@gmail.com>
2026-02-22 16:06:15 -0500

    msg

--- src/data/exams.ts
+++ src/data/exams.ts
@@ -356,9 +356,35 @@ public class Spring2021Question2 {
         id: "question-5",
         title: "question 5",
         topics: ["Arrays"],
-        prompt: "Question prompt goes here.",
+        prompt: `You are playing a game that has two dice – one die has 6 sides numbered 1 through 6 and the second die has 8 sides numbered 1 through 8. A turn involves rolling both die together.
+
+Write Java code to play 1,000 turns and keep track of the sum of the two dice in each roll i.e, (2 through 14).
+
+When all 1,000 turns have been completed, print a formatted table showing the value of the sum of the two dice (2 through 14) and the number of times that value occurred.`,
         explanation: "Explanation goes here.",
-        solution: "// Solution code goes here",
+        solution: `public class Spring2021Question5 {
+    public static void main(String[] args) {
+        final int NUM_ROLLS = 1000;
+        final int MAX_ROLL = 14;
+
+        int[] histogram = new int[MAX_ROLL + 1];
+
+        for (int i = 0; i < NUM_ROLLS; i++) {
+            int dieOneRoll = (int) (Math.random() * (6 - 1 + 1) + 1);
+            int dieTwoRoll = (int) (Math.random() * (8 - 1 + 1) + 1);
+
+            int roll = dieOneRoll + dieTwoRoll;
+
+            histogram[roll]++;
+        }
+
+        System.out.printf("%8s%8s\\n", "Dice Sum", "Count");
+
+        for (int i = 2; i < histogram.length; i++) {
+            System.out.printf("%8d%8d\\n", i, histogram[i]);
+        }
+    }
+}`,
       },
       {
         id: "question-6",
